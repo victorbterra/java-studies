a00959dc5f2742d8fa23fab971bf674159ab967e
Victor Terra <victorterradev@gmail.com>
2025-05-23 17:28:39 -0300

    Meu primeiro hello world em Java

---
+++ src/semana01/HelloWorld.java
@@ -0,0 +1,8 @@
+package semana01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String greeting = "My first Hello World !";
+        System.out.println(greeting);
+    }
+}
