1a018f6239311ea2473850a5dee7b6e08295f2e7
Victor Terra <victorterradev@gmail.com>
2025-05-26 14:57:11 -0300

    Operadores lógicos

--- src/semana01/Operadores.java
+++ src/semana01/Operadores.java
@@ -18,6 +18,27 @@ public class Operadores {
         int resto = a % b;
         System.out.println("A soma de "+a+"com "+b+" é "+soma);
         System.out.println("O resto da divisão "+a+"com "+b+" é "+resto);
+        /*
+        OPERADORES DE COMPARAÇÃO(QUE RETORNAM BOOLEAN)
+        == //   igual a
+        != //   diferente de
+        >  //    maior que
+        <  //    menor que
+        >= //    maior ou igual
+        <= //    menor ou igual
+        */
+        boolean resultado = (10 > 5); // o resultado retorna true
+        System.out.println("A Condição da operação resultado é: "+resultado);
+        /*
+        OPERADORES LÓGICOS (USADOS COM BOOLEANOS)
+        && //   E (and)
+        || //   OU (or)
+        !  //   NÃO(not)
+        */
+        //EXEMPLO
+        boolean ligado = true;
+        boolean funcionando = false;
+        boolean status = ligado && funcionando; // false
     }
 
 }
